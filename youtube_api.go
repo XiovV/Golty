e614745a892d5ea7e58c1f3d5c4b2dcea6e33d69
XiovV <xiovv@tutanota.com>
2019-11-03 09:17:06 +0100

    removed commented code

--- youtube_api.go
+++ youtube_api.go
@@ -55,31 +55,6 @@ func GetLatestVideo(channelName, channelType string) (string, string) {
 	return video.Items[0].ID.VideoID, video.Items[0].Snippet.Title
 }
 
-// func GetChannelVideoData(channelId string) (string, string) {
-// 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-// 	requestURL := API_ENDPOINT_ID + channelId + "&" + MAX_RESULTS + "&" + ORDER_BY + "&" + TYPE + "&key=" + API_KEY
-
-// 	resp, err := http.Get(requestURL)
-
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	defer resp.Body.Close()
-
-// 	body, err := ioutil.ReadAll(resp.Body)
-
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	var video ChannelBody
-
-// 	json.Unmarshal([]byte(string(body)), &video)
-
-// 	return video.Items[0].ID.VideoID, video.Items[0].Snippet.Title
-// }
-
 func getUserUploadsID(channelName string) string {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	requestURL := API_ENDPOINT_NAME + channelName + "&key=" + API_KEY
@@ -104,29 +79,3 @@ func getUserUploadsID(channelName string) string {
 
 	return user.Items[0].ContentDetails.RelatedPlaylists.Uploads
 }
-
-// func GetUserVideoData(channelName string) (string, string) {
-// 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-// 	uploadsId := getUserUploadsID(channelName)
-// 	requestURL := API_ENDPOINT_PLAYLIST + uploadsId + "&maxResults=2" + "&key=" + API_KEY
-
-// 	resp, err := http.Get(requestURL)
-
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	defer resp.Body.Close()
-
-// 	body, err := ioutil.ReadAll(resp.Body)
-
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	var video NameBody
-
-// 	json.Unmarshal([]byte(string(body)), &video)
-
-// 	return video.Items[0].Snippet.ResourceID.VideoID, video.Items[0].Snippet.Title
-// }
