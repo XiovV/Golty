a2ad8ac5644eaf03b37a05897c9418f4eacf54e7
XiovV <xiovv@tutanota.com>
2019-11-08 14:36:38 +0100

    removed commented code

--- downloader.go
+++ downloader.go
@@ -21,6 +21,23 @@ import (
 
 // THIS DOWNLOADER IS BASED ON https://github.com/knadh/go-get-youtube
 
+func DownloadVideoAndAudio(channelName, channelType string) {
+	videoId, videoTitle := GetLatestVideo(channelName, channelType)
+	path := channelName + "/" + videoTitle + ".mp4"
+
+	video, err := getVideoData(videoId)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	option := &Option{
+		Rename: false,
+		Resume: true,
+		Mp3:    true,
+	}
+	video.Download(0, path, option)
+}
+
 func DownloadAudio(videoID, videoTitle, channelName string) {
 	path := channelName + "/" + videoTitle + ".mp4"
 
@@ -122,23 +139,6 @@ func getVideoData(videoId string) (Video, error) {
 	}
 
 	return *meta, nil
-}
-
-func DownloadVideoAndAudio(channelName, channelType string) {
-	videoId, videoTitle := GetLatestVideo(channelName, channelType)
-	path := channelName + "/" + videoTitle + ".mp4"
-
-	video, err := getVideoData(videoId)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	option := &Option{
-		Rename: false,
-		Resume: true,
-		Mp3:    true,
-	}
-	video.Download(0, path, option)
 }
 
 func (video *Video) Download(index int, filename string, option *Option) error {
@@ -319,66 +319,3 @@ func printProgress(out *os.File, offset, length int64) {
 		}
 	}
 }
-
-// func DownloadAudio(videoID, videoTitle, channelName string) {
-// 	path := channelName + "/" + videoTitle + ".mp4"
-
-// 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-// 	video, err := youtube.Get(videoID)
-// 	if err != nil {
-// 		log.Panic(err)
-// 	}
-
-// 	option := &youtube.Option{
-// 		Rename: false,
-// 		Resume: true,
-// 		Mp3:    true,
-// 	}
-// 	video.Download(0, path, option)
-// 	fmt.Println("Removing mp4...")
-// 	os.Remove(path)
-// }
-
-// func DownloadVideo(videoID, videoTitle, channelName string) {
-// 	path := channelName + "/" + videoTitle + ".mp4"
-// 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-// 	video, err := youtube.Get(videoID)
-// 	if err != nil {
-// 		log.Panic(err)
-// 	}
-
-// 	option := &youtube.Option{
-// 		Rename: false,
-// 		Resume: true,
-// 		Mp3:    false,
-// 	}
-// 	video.Download(0, path, option)
-// }
-
-// func Download(channelName, channelType, downloadMode string) {
-// 	if channelType == "user" {
-// 		videoId, videoTitle := GetLatestVideo(channelName, channelType)
-// 		if downloadMode == "Audio Only" {
-// 			DownloadAudio(videoId, videoTitle, channelName)
-// 			UpdateLatestDownloaded(channelName, videoId)
-// 		} else if downloadMode == "Video Only" {
-// 			UpdateLatestDownloaded(channelName, videoId)
-// 			DownloadVideo(videoId, videoTitle, channelName)
-// 		} else if downloadMode == "Video And Audio" {
-// 			UpdateLatestDownloaded(channelName, videoId)
-// 			DownloadVideo(videoId, videoTitle, channelName)
-// 		}
-// 	} else if channelType == "channel" {
-// 		videoId, videoTitle := GetLatestVideo(channelName, channelType)
-// 		if downloadMode == "Audio Only" {
-// 			UpdateLatestDownloaded(channelName, videoId)
-// 			DownloadAudio(videoId, videoTitle, channelName)
-// 		} else if downloadMode == "Video Only" {
-// 			UpdateLatestDownloaded(channelName, videoId)
-// 			DownloadVideo(videoId, videoTitle, channelName)
-// 		} else if downloadMode == "Video And Audio" {
-// 			UpdateLatestDownloaded(channelName, videoId)
-// 			DownloadVideo(videoId, videoTitle, channelName)
-// 		}
-// 	}
-// }
