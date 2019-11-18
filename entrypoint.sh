11fa919223356269613e6fc49c999dde8c2db2cd
Josh Jacoba <josh@joshjacobs.net>
2019-11-18 16:47:12 +0000

    Set UMASK for ASH in Entrypoint
    Otherwise it seems to use the original system UMASK, even if changed. ASH is odd.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 
 DEFAULT_UID=1000
 DEFAULT_GID=1000
+DEFAULT_UMASK=022
 
 usermod goautoyt -u ${PUID:-${DEFAULT_UID}}
 groupmod goautoyt -g ${PGID:-${DEFAULT_GID}}
 
 chown -R goautoyt:goautoyt /app
-exec su -s /bin/ash -c "PATH=$PATH:/usr/local/bin;./main" goautoyt
+exec su -s /bin/ash -c "umask ${UMASK_SET:-${DEFAULT_UMASK}};PATH=$PATH:/usr/local/bin;./main" goautoyt
