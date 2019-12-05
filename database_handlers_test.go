a571e187ce3c6cc9377b3635ad758ba054bf7bfb
XiovV <xiovv@tutanota.com>
2019-12-05 15:49:03 +0100

    removed all unit tests except AddToDatabse for now

--- database_handlers_test.go
+++ database_handlers_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
-	"time"
 )
 
 var targets = []DownloadTarget{
@@ -57,67 +56,67 @@ func TestDownloadTarget_AddToDatabase(t *testing.T) {
 		assert.Nil(err)
 	}
 }
-
-func TestDownloadTarget_UpdateLatestDownloaded(t *testing.T) {
-	assert := assert.New(t)
-	for _, target := range targets {
-		err := target.AddToDatabase()
-		assert.Nil(err)
-		err = target.UpdateLatestDownloaded("newvideoid123")
-		assert.Nil(err)
-		updatedTarget, err := target.GetFromDatabase()
-		assert.Nil(err)
-		assert.Equal("newvideoid123", updatedTarget.LatestDownloaded, "updatedTarget.LatestDownloaded should be newvideoid123")
-		err = updatedTarget.Delete()
-		assert.Nil(err)
-	}
-}
-
-func TestUpdateCheckingInterval(t *testing.T) {
-	assert := assert.New(t)
-	for _, target := range targets {
-		err := target.AddToDatabase()
-		assert.Nil(err)
-		_, err = target.UpdateCheckingInterval("5")
-		assert.Nil(err)
-		updatedTarget, err := target.GetFromDatabase()
-		assert.Equal("5", updatedTarget.CheckingInterval, "updatedTarget.CheckingInterval should be 5")
-		err = updatedTarget.Delete()
-		assert.Nil(err)
-	}
-}
-
-func TestGetCheckingInterval(t *testing.T) {
-	assert := assert.New(t)
-	for _, target := range targets {
-		if target.Name != "Heroic Motivational Anthems" {
-			err := target.AddToDatabase()
-			assert.Nil(err)
-			_, err = target.UpdateCheckingInterval("5")
-			assert.Nil(err)
-			checkingInterval, err := GetCheckingInterval("channels")
-			assert.Nil(err)
-			assert.Equal(5, checkingInterval, "checkingInterval should be 5 for"+target.Name)
-			err = target.Delete()
-			assert.Nil(err)
-		}
-	}
-}
-
-func TestDownloadTarget_UpdateLastChecked(t *testing.T) {
-	currTime := time.Now().Format("01-02-2006 15:04:05")
-	assert := assert.New(t)
-	for _, target := range targets {
-		if target.Name != "Heroic Motivational Anthems" {
-			err := target.AddToDatabase()
-			assert.Nil(err)
-			err = target.UpdateLastChecked()
-			assert.Nil(err)
-			updatedTarget, err := target.GetFromDatabase()
-			assert.Nil(err)
-			assert.Equal(currTime, updatedTarget.LastChecked, "updatedTarget.LastChecked should be "+currTime)
-			err = target.Delete()
-			assert.Nil(err)
-		}
-	}
-}
+//
+//func TestDownloadTarget_UpdateLatestDownloaded(t *testing.T) {
+//	assert := assert.New(t)
+//	for _, target := range targets {
+//		err := target.AddToDatabase()
+//		assert.Nil(err)
+//		err = target.UpdateLatestDownloaded("newvideoid123")
+//		assert.Nil(err)
+//		updatedTarget, err := target.GetFromDatabase()
+//		assert.Nil(err)
+//		assert.Equal("newvideoid123", updatedTarget.LatestDownloaded, "updatedTarget.LatestDownloaded should be newvideoid123")
+//		err = updatedTarget.Delete()
+//		assert.Nil(err)
+//	}
+//}
+//
+//func TestUpdateCheckingInterval(t *testing.T) {
+//	assert := assert.New(t)
+//	for _, target := range targets {
+//		err := target.AddToDatabase()
+//		assert.Nil(err)
+//		_, err = target.UpdateCheckingInterval("5")
+//		assert.Nil(err)
+//		updatedTarget, err := target.GetFromDatabase()
+//		assert.Equal("5", updatedTarget.CheckingInterval, "updatedTarget.CheckingInterval should be 5")
+//		err = updatedTarget.Delete()
+//		assert.Nil(err)
+//	}
+//}
+//
+//func TestGetCheckingInterval(t *testing.T) {
+//	assert := assert.New(t)
+//	for _, target := range targets {
+//		if target.Name != "Heroic Motivational Anthems" {
+//			err := target.AddToDatabase()
+//			assert.Nil(err)
+//			_, err = target.UpdateCheckingInterval("5")
+//			assert.Nil(err)
+//			checkingInterval, err := GetCheckingInterval("channels")
+//			assert.Nil(err)
+//			assert.Equal(5, checkingInterval, "checkingInterval should be 5 for"+target.Name)
+//			err = target.Delete()
+//			assert.Nil(err)
+//		}
+//	}
+//}
+//
+//func TestDownloadTarget_UpdateLastChecked(t *testing.T) {
+//	currTime := time.Now().Format("01-02-2006 15:04:05")
+//	assert := assert.New(t)
+//	for _, target := range targets {
+//		if target.Name != "Heroic Motivational Anthems" {
+//			err := target.AddToDatabase()
+//			assert.Nil(err)
+//			err = target.UpdateLastChecked()
+//			assert.Nil(err)
+//			updatedTarget, err := target.GetFromDatabase()
+//			assert.Nil(err)
+//			assert.Equal(currTime, updatedTarget.LastChecked, "updatedTarget.LastChecked should be "+currTime)
+//			err = target.Delete()
+//			assert.Nil(err)
+//		}
+//	}
+//}
