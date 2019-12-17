adb95fbe9f407459d100ef9726352cc307b2497b
Josh Jacobs <josh@joshjacobs.net>
2019-12-17 23:04:31 +0000

    Build Images Without Pushing for Pull Requests

--- travis/build-amd64.sh
+++ travis/build-amd64.sh
@@ -12,10 +12,17 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 
     buildctl build --frontend dockerfile.v0 \
         --frontend-opt platform=linux/${PLATFORM} \
-        --frontend-opt filename=./${DOCKERFILE_LOCATION} \
+        --frontend-opt filename=${DOCKERFILE_LOCATION} \
         --exporter image \
         --exporter-opt name=docker.io/${DOCKER_USER}/${IMAGE}:${TAG}-${PLATFORM} \
         --exporter-opt push=true \
+        --local dockerfile=. \
+        --local context=.
+else
+    buildctl build --frontend dockerfile.v0 \
+        --frontend-opt platform=linux/${PLATFORM} \
+        --frontend-opt filename=${DOCKERFILE_LOCATION} \
+        --exporter image \
         --local dockerfile=. \
         --local context=.
 fi
