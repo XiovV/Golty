f5d49919e47e02cca6d4fbffc51af1ef4147f023
Josh Jacobs <josh@joshjacobs.net>
2019-12-19 22:59:56 +0000

    Add MultiArch Docker Build Support
    For:
    - ARM
    - ARM64
    - AMD64

--- travis/dockerhub-deploy.sh
+++ travis/dockerhub-deploy.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -ev
-docker build --build-arg TRAVIS_PULL_REQUEST -t go-auto-yt .
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-    docker tag go-auto-yt xiovv/go-auto-yt:stable
-    docker push xiovv/go-auto-yt:stable
+
+    docker manifest create ${DOCKER_USER}/go-auto-yt:stable \
+            ${DOCKER_USER}/go-auto-yt:stable-amd64 \
+            ${DOCKER_USER}/go-auto-yt:stable-arm \
+            ${DOCKER_USER}/go-auto-yt:stable-arm64
+
+    docker manifest annotate ${DOCKER_USER}/go-auto-yt:stable ${DOCKER_USER}/go-auto-yt:stable-amd64 --arch amd64
+    docker manifest annotate ${DOCKER_USER}/go-auto-yt:stable ${DOCKER_USER}/go-auto-yt:stable-arm --arch arm
+    docker manifest annotate ${DOCKER_USER}/go-auto-yt:stable ${DOCKER_USER}/go-auto-yt:stable-arm64 --arch arm64
+
+    docker manifest push ${DOCKER_USER}/go-auto-yt:stable
+else
+    docker images
 fi
