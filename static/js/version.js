d0fd625f3841b1de7708a3e86b26bc60c6d28638
XiovV <xiovv@tutanota.com>
2023-11-24 11:41:43 +0100

    delete old code

--- static/js/version.js
+++
@@ -1,9 +0,0 @@
-function getVersion() {
-    let version = document.getElementById("version-number")
-
-    fetch("/api/version")
-        .then(res => res.json())
-        .then(res => {
-            version.innerText = res.VersionNumber
-        })
-}
