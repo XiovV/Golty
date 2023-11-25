77864d9b730f391f23164a65a77ce4a30ec068a1
XiovV <xiovv@tutanota.com>
2023-11-25 21:32:03 +0100

    fix(client): set dark backround colour only on large screens

--- src/app/components/navigation/nav-item.tsx
+++ src/app/components/navigation/nav-item.tsx
@@ -18,7 +18,7 @@ export default function NavItem({ link, name, isActive, icon }: NavItemProps) {
       className={clsx(
         "flex flex-col justify-center items-center text-[#676D75] text-sm font-semibold lg:flex-row lg:justify-normal lg:text-lg lg:py-3 lg:px-4 lg:mb-1 lg:rounded-[16px] lg:gap-3 lg:hover:bg-[#292e37]",
         {
-          "text-[#ffffff] bg-[#292e37]": isActive,
+          "text-[#ffffff] lg:bg-[#292e37]": isActive,
         }
       )}
     >
