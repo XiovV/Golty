5a2dfe2833d07351d3a786463171eeb140c4debe
XiovV <xiovv@tutanota.com>
2023-11-25 21:28:39 +0100

    feat(client): display channel avatar

--- src/app/channels/[channelName]/page.tsx
+++ src/app/channels/[channelName]/page.tsx
@@ -4,10 +4,26 @@ import { IoSearch } from "react-icons/io5";
 import { MdOutlineSort } from "react-icons/md";
 import { LuRefreshCw } from "react-icons/lu";
 import { IoMdSettings } from "react-icons/io";
+import { Channel as IChannel } from "@/app/types/channel";
 
 import { FiTrash } from "react-icons/fi";
+import Channel from "@/app/components/channel";
 
-export default function Page({ params }: { params: { channelName: string } }) {
+async function fetchChannel(channelName: string): Promise<IChannel> {
+  const res = await fetch(`${process.env.API_URL}/channels/${channelName}`, {
+    cache: "no-store",
+  });
+
+  return await res.json();
+}
+
+export default async function Page({
+  params,
+}: {
+  params: { channelName: string };
+}) {
+  const channel = await fetchChannel(params.channelName);
+
   return (
     <main>
       <TopBar
@@ -15,6 +31,15 @@ export default function Page({ params }: { params: { channelName: string } }) {
         mobileIcons={[IoSearch, LuRefreshCw, MdOutlineSort, IoMdSettings]}
         desktopIcons={[LuRefreshCw, MdOutlineSort, IoMdSettings, FiTrash]}
       />
+
+      <div className="m-5">
+        <Channel
+          avatarUrl={channel.avatarUrl}
+          name={channel.name}
+          totalVideos={channel.totalVideos}
+          totalSize={channel.totalSize}
+        />
+      </div>
     </main>
   );
 }
