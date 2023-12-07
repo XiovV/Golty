a22f0d72db2f8ffa81a23be1f86b63a083f4bf73
XiovV <xiovv@tutanota.com>
2023-12-07 16:27:20 +0100

    fix(client): add target _blank to channel info card links

--- src/components/channel/ChannelInfoCard.tsx
+++ src/components/channel/ChannelInfoCard.tsx
@@ -16,16 +16,16 @@ export default function ChannelInfoCard({
 }: ChannelInfoCardProps) {
   return (
     <div className="flex gap-3 text-white">
-      <Link href={channelUrl}>
+      <Link href={channelUrl} target="_blank">
         <ChannelAvatar avatarUrl={avatarUrl} size={85} />
       </Link>
 
       <div className="flex flex-col justify-between text-lg">
         <div className="flex flex-col gap-1">
-          <Link href={channelUrl}>
+          <Link href={channelUrl} target="_blank">
             <p>{name}</p>
           </Link>
-          <Link href={channelUrl}>
+          <Link href={channelUrl} target="_blank">
             <p className="text-[#676D75] text-sm">{handle}</p>
           </Link>
         </div>
