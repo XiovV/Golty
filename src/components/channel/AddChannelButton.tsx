9d9260b482ecd66504c8a113646c8a6694440aa5
XiovV <xiovv@tutanota.com>
2023-12-11 18:37:47 +0100

    feat(client): set file extensions based on download options

--- src/components/channel/AddChannelButton.tsx
+++ src/components/channel/AddChannelButton.tsx
@@ -30,6 +30,7 @@ import {
   useAddChannel,
   useFetchChannelInfo,
 } from "@/hooks/channel/channelHooks";
+import { useState } from "react";
 
 export default function AddChannelButton() {
   return (
@@ -50,14 +51,28 @@ export default function AddChannelButton() {
   );
 }
 
+const videoExtensions: string[] = ["m4a", "mp4", "webm", "flv", "ogg", "3gp"];
+const audioExtensions: string[] = [
+  "aac",
+  "alac",
+  "flac",
+  "mp3",
+  "opus",
+  "vorbis",
+  "wav",
+];
+
 function AddChannelForm() {
+  const [downloadExtensions, setDownloadExtensions] =
+    useState<string[]>(videoExtensions);
   const { loading, channelInfo, getChannelInfo } = useFetchChannelInfo();
   const { addChannel } = useAddChannel();
+
   const channelUrlRef = useRef<HTMLInputElement>(null);
 
   return (
     <form
-      onSubmit={(e) => addChannel(e, channelInfo)}
+      onSubmit={(e) => addChannel(e, channelInfo!)}
       className="flex flex-col gap-6"
     >
       <div className="grid w-full max-w-sm items-center gap-2">
@@ -79,17 +94,110 @@ function AddChannelForm() {
           avatarUrl={channelInfo.avatar.url}
           name={channelInfo.uploader}
           handle={channelInfo.uploader_id}
-          channelUrl={channelUrlRef.current?.value!}
+          channelUrl={channelUrlRef.current!.value!}
         />
       )}
 
       <div className="flex flex-col gap-6">
         <div className="flex flex-row justify-between">
-          <AddChannelFormCheckboxGroup disabled={!channelInfo} />
-          <AddChannelFormSelectGroup disabled={!channelInfo} />
+          <div className="flex flex-col gap-2 ">
+            <div className="flex items-center space-x-2">
+              <CheckboxShadcn
+                id="video"
+                defaultChecked
+                name="video"
+                disabled={!channelInfo}
+                onCheckedChange={(checked) => {
+                  if (checked) {
+                    setDownloadExtensions(videoExtensions);
+                    return;
+                  }
+
+                  setDownloadExtensions(videoExtensions);
+                }}
+              />
+              <label
+                htmlFor={"video"}
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Video
+              </label>
+            </div>
+
+            <div className="flex items-center space-x-2">
+              <CheckboxShadcn
+                id="audio"
+                defaultChecked
+                name="audio"
+                disabled={!channelInfo}
+              />
+              <label
+                htmlFor="audio"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Audio
+              </label>
+            </div>
+          </div>
+
+          <div className="flex gap-2">
+            <Select name="format" defaultValue="auto" disabled={!channelInfo}>
+              <SelectTrigger className="w-[90px]">
+                <SelectValue placeholder="Auto" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Format</SelectLabel>
+                  <SelectItem value="auto">Auto</SelectItem>
+
+                  {downloadExtensions.map((extension) => (
+                    <SelectItem key={extension} value={extension}>
+                      {extension}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+
+            <Select
+              name="resolution"
+              defaultValue="2160p"
+              disabled={!channelInfo}
+            >
+              <SelectTrigger className="w-[90px]">
+                <SelectValue placeholder="2160p" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Resolution</SelectLabel>
+                  <SelectItem value="2160p">2160p</SelectItem>
+                  <SelectItem value="1440p">1440p</SelectItem>
+                  <SelectItem value="1080p">1080p</SelectItem>
+                  <SelectItem value="720p">720p</SelectItem>
+                  <SelectItem value="480p">480p</SelectItem>
+                  <SelectItem value="360p">360p</SelectItem>
+                  <SelectItem value="240p">240p</SelectItem>
+                  <SelectItem value="144p">144p</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
         </div>
 
-        <AddChannelFormSwitchGroup disabled={!channelInfo} />
+        <div className="flex flex-col gap-2">
+          <Switch
+            label="Automatically download new uploads"
+            name="downloadAutomatically"
+            disabled={!channelInfo}
+            defaultChecked
+          />
+          <Switch
+            label="Download the entire channel"
+            name="downloadEntireChannel"
+            disabled={!channelInfo}
+            defaultChecked
+          />
+        </div>
       </div>
 
       <DialogFooter>
@@ -120,112 +228,6 @@ function Switch({ label, defaultChecked, disabled, name }: SwitchProps) {
         defaultChecked={defaultChecked}
       />
       <Label htmlFor={label}>{label}</Label>
-    </div>
-  );
-}
-
-interface SwitchGroupProps {
-  disabled: boolean;
-}
-
-function AddChannelFormSwitchGroup({ disabled }: SwitchGroupProps) {
-  return (
-    <div className="flex flex-col gap-2">
-      <Switch
-        label="Automatically download new uploads"
-        name="downloadAutomatically"
-        disabled={disabled}
-        defaultChecked
-      />
-      <Switch
-        label="Download the entire channel"
-        name="downloadEntireChannel"
-        disabled={disabled}
-        defaultChecked
-      />
-    </div>
-  );
-}
-
-interface CheckboxGroupProps {
-  disabled?: boolean;
-}
-
-function AddChannelFormCheckboxGroup({ disabled }: CheckboxGroupProps) {
-  return (
-    <div className="flex flex-col gap-2 ">
-      <Checkbox label="Video" name="video" disabled={disabled} defaultChecked />
-      <Checkbox label="Audio" name="audio" disabled={disabled} defaultChecked />
-    </div>
-  );
-}
-
-interface CheckboxProps {
-  label: string;
-  name: string;
-  defaultChecked?: boolean;
-  disabled?: boolean;
-}
-
-function Checkbox({ label, defaultChecked, disabled, name }: CheckboxProps) {
-  return (
-    <div className="flex items-center space-x-2">
-      <CheckboxShadcn
-        id={label}
-        defaultChecked={defaultChecked}
-        name={name}
-        disabled={disabled}
-      />
-      <label
-        htmlFor={label}
-        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-      >
-        {label}
-      </label>
-    </div>
-  );
-}
-
-interface SelectGroupProps {
-  disabled?: boolean;
-}
-
-function AddChannelFormSelectGroup({ disabled }: SelectGroupProps) {
-  return (
-    <div className="flex gap-2">
-      <Select name="format" defaultValue="auto" disabled={disabled}>
-        <SelectTrigger className="w-[90px]">
-          <SelectValue placeholder="Auto" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Format</SelectLabel>
-            <SelectItem value="auto">Auto</SelectItem>
-            <SelectItem value="m4a">m4a</SelectItem>
-            <SelectItem value="mp3">mp3</SelectItem>
-            <SelectItem value="mp4">mp4</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-
-      <Select name="resolution" defaultValue="2160p" disabled={disabled}>
-        <SelectTrigger className="w-[90px]">
-          <SelectValue placeholder="2160p" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Resolution</SelectLabel>
-            <SelectItem value="2160p">2160p</SelectItem>
-            <SelectItem value="1440p">1440p</SelectItem>
-            <SelectItem value="1080p">1080p</SelectItem>
-            <SelectItem value="720p">720p</SelectItem>
-            <SelectItem value="480p">480p</SelectItem>
-            <SelectItem value="360p">360p</SelectItem>
-            <SelectItem value="240p">240p</SelectItem>
-            <SelectItem value="144p">144p</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
     </div>
   );
 }
