5df7cdb5bc2515f034f17f9071b3d7a8a85f7acc
XiovV <xiovv@tutanota.com>
2023-12-12 20:50:52 +0100

    refactor(server): remove the DownloadVideo func

--- server/ytdl/ytdl.go
+++ server/ytdl/ytdl.go
@@ -74,13 +74,3 @@ func (y *Ytdl) jq(input string, output any, args ...string) error {
 
 	return nil
 }
-
-func (y *Ytdl) DownloadVideo(videoId string, options VideoDownloadOptions) error {
-	if options.Video {
-		resolutionFlag := fmt.Sprintf("res:%s", y.resolutions[options.Resolution])
-		_, err := y.exec("-S", resolutionFlag, "-o", options.Output, videoId)
-		return err
-	}
-
-	return nil
-}
