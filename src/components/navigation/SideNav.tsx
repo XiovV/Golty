40f22c96391ace75181790ca69383fd608aade69
XiovV <xiovv@tutanota.com>
2023-12-22 11:23:14 +0100

    fix(client): redirect to the homepage after clicking on the Golty logo

--- src/components/navigation/SideNav.tsx
+++ src/components/navigation/SideNav.tsx
@@ -11,6 +11,7 @@ import { MdVideoLibrary } from "react-icons/md";
 import { HiUsers } from "react-icons/hi";
 import { MdBugReport } from "react-icons/md";
 import { FaBell } from "react-icons/fa";
+import Link from "next/link";
 
 const libraryLinks = [
   { name: "Channels", href: "/dashboard/channels", icon: PiTelevisionFill },
@@ -29,10 +30,10 @@ export default function SideNav() {
   const pathname = usePathname();
   return (
     <div className="h-full px-6 py-4 bg-[#1D1F24]">
-      <div className="flex items-center gap-3 pb-3">
+      <Link href="/" className="flex items-center gap-3 pb-3">
         <Image priority src={GoltyLogo} alt="Golty Logo" />
         <p className="text-white text-2xl font-bold">Golty</p>
-      </div>
+      </Link>
 
       <p className="text-[#676D75] font-semibold pb-3">Library</p>
 
