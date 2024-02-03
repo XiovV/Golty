e9418d6ce11016f210e00070f93ff7d8887b3678
XiovV <xiovv@tutanota.com>
2024-02-03 20:16:17 +0100

    chore: fix the merge issue

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-<<<<<<< HEAD
 
-=======
- 
->>>>>>> parent of e08ff49... chore(client): remove next.js files
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
